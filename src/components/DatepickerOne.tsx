1ca753f5e1c8f35177f77a8a89a287b9e1ba07f2
DESKTOP-BF3VM9G\John Mandy Samonte <johnmandysamonte@gmail.com>
2023-03-19 02:49:47 +0800

    Added datepicker multi type

--- src/components/DatepickerOne.tsx
+++ src/components/DatepickerOne.tsx
@@ -1,8 +1,9 @@
 import { useMonth } from "@datepicker-react/hooks";
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 import { useLilius } from "use-lilius";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import classNames from "classnames";
+import { BsArrowRightShort } from "react-icons/bs";
 import {
     addWeeks,
     compareAsc,
@@ -11,6 +12,7 @@ import {
     endOfWeek,
     format,
     getDay,
+    isBefore,
     isEqual,
     isToday,
     startOfMonth,
@@ -32,22 +34,23 @@ function Days() {
     );
 }
 
-interface DatepickerProps {}
+interface DatepickerProps {
+    type?: "default" | "multi" | "range";
+}
 
-export default function DatepickerOne({}: DatepickerProps) {
+export default function DatepickerOne({ type = "default" }: DatepickerProps) {
     const [isVisible, setVisible] = useState(false);
     const {
         calendar,
+        selected,
         deselect,
         inRange,
         isSelected,
         select,
-        selected,
         selectRange,
-        viewing,
         viewNextMonth,
         viewPreviousMonth,
-        viewToday,
+        toggle,
     } = useLilius({ numberOfMonths: 2 });
     const firstActiveMonth = useMemo(() => calendar[0], [calendar]);
     const secondActiveMonth = useMemo(() => calendar[1], [calendar]);
@@ -55,43 +58,58 @@ export default function DatepickerOne({}: DatepickerProps) {
     const firstSundayOfFirstMonth = useMemo(() => calendar[0][0][6], [calendar]);
     const firstSundayOfSecondMonth = useMemo(() => calendar[1][0][6], [calendar]);
 
-    const onClickDate = (date: Date) => {
+    const datesClassName = useCallback(
+        (date: Date, firstSunday: Date) => {
+            return classNames("w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-zinc-100", {
+                "text-zinc-400": !inRange(date, startOfMonth(firstSunday), endOfMonth(firstSunday)),
+                "!bg-primary !text-white":
+                    (type === "range" &&
+                        (isEqual(date, selected[0]) || isEqual(date, selected[selected.length - 1]))) ||
+                    (type === "multi" && isSelected(date)),
+                "bg-zinc-200": type === "range" && isSelected(date),
+                "border border-primary text-primary": isToday(date),
+            });
+        },
+        [selected]
+    );
+
+    const selectDateRange = (date: Date) => {
         const sorted = selected.sort((a, b) => compareAsc(a, b));
 
         if (sorted.length === 0) {
-            // Check if there's an item in sorted or selected
-            // If sorted is empty, then add the date as selected
-            // select function only stores 1 date at a time
-
             select(date);
-        } else if (isSelected(date)) {
-            // if the date is ALREADY selected
-
-            if (selected.length === 1) {
-                // if you select the start date again, you will deselect the range
-                deselect(date);
-            } else {
-                //
-                const range = eachDayOfInterval({ start: sorted[0], end: date });
-                console.log(range);
-                const diff = sorted.filter((d) => range.map((a) => a.getTime()).includes(d.getTime()));
-
-                selectRange(diff[0], diff[diff.length - 1], true);
-            }
+        } else if (isBefore(date, selected[0])) {
+            selectRange(date, sorted[0], true);
+        } else if (isSelected(date) && selected.length === 1) {
+            deselect(date);
         } else {
-            //
             selectRange(sorted[0], date, true);
         }
     };
 
-    useEffect(() => {
-        console.log(firstSundayOfSecondMonth);
-    }, []);
+    const onSelect = (date: Date) => {
+        switch (type) {
+            case "multi":
+                return toggle(date);
+            case "range":
+                return selectDateRange(date);
+            default:
+                return;
+        }
+    };
 
     return (
         <div className="relative">
             <div className="px-5 py-3 rounded-lg border border-primary bg-white">
-                <span>Select Date</span>
+                {selected?.length === 0 ? (
+                    <span>Select Date</span>
+                ) : (
+                    <span className="flex items-center gap-x-2">
+                        {selected[0] && format(selected[0], "dd-MM-yyyy")}{" "}
+                        <BsArrowRightShort className="text-primary" />{" "}
+                        {selected[selected.length - 1] && format(selected[selected.length - 1], "dd-MM-yyyy")}
+                    </span>
+                )}
             </div>
 
             <div className="absolute top-full mt-2 bg-white rounded-lg shadow-md flex flex-col p-5">
@@ -104,7 +122,9 @@ export default function DatepickerOne({}: DatepickerProps) {
                     </button>
 
                     {calendar.map(([firstWeek]) => (
-                        <span key={firstWeek[6].toDateString()}>{format(firstWeek[6], "MMMM yyyy")}</span>
+                        <span key={firstWeek[6].toDateString()} className="font-medium">
+                            {format(firstWeek[6], "MMMM yyyy")}
+                        </span>
                     ))}
 
                     <button
@@ -115,24 +135,32 @@ export default function DatepickerOne({}: DatepickerProps) {
                     </button>
                 </div>
 
-                {/* <div className="grid grid-cols-2 gap-x-5">
+                {/* <div className="grid grid-cols-2 gap-x-5 ">
                     {calendar.map((month) => (
-                        <div className="grid grid-cols-7">
-                            <Days />
-
-                            {flatten(month).map((day) => (
+                        <div key={format(firstSundayOfFirstMonth, "MMMM-yyyy")} className="grid grid-cols-7">
+                            {flatten(month).map((date) => (
                                 <div
-                                    key={format(day, "dd-MM-yyyy")}
+                                    key={format(date, "dd-MM-yyyy")}
+                                    onClick={() => onSelectDateRange(date)}
                                     className={classNames("w-10 h-10 flex items-center justify-center", {
-                                        "text-zinc-300": !inRange(
-                                            day,
-                                            startOfMonth(firstSundayOfFirstMonth),
-                                            endOfMonth(firstSundayOfSecondMonth)
-                                        ),
-                                        "bg-primary text-white": isToday(day),
+                                        "text-zinc-400":
+                                            !inRange(
+                                                date,
+                                                startOfMonth(firstSundayOfFirstMonth),
+                                                endOfMonth(firstSundayOfFirstMonth)
+                                            ) &&
+                                            !inRange(
+                                                date,
+                                                startOfMonth(firstSundayOfSecondMonth),
+                                                endOfMonth(firstSundayOfSecondMonth)
+                                            ),
+                                        "!bg-primary !text-white":
+                                            isEqual(date, selected[0]) || isEqual(date, selected[selected.length - 1]),
+                                        "bg-zinc-200": isSelected(date),
+                                        "border border-primary text-primary": isToday(date),
                                     })}
                                 >
-                                    {format(day, "dd")}
+                                    {format(date, "dd")}
                                 </div>
                             ))}
                         </div>
@@ -147,18 +175,8 @@ export default function DatepickerOne({}: DatepickerProps) {
                             {flatten(firstActiveMonth).map((date) => (
                                 <div
                                     key={format(date, "dd-MM-yyyy")}
-                                    onClick={() => onClickDate(date)}
-                                    className={classNames("w-10 h-10 flex items-center justify-center", {
-                                        "text-zinc-400": !inRange(
-                                            date,
-                                            startOfMonth(firstSundayOfFirstMonth),
-                                            endOfMonth(firstSundayOfFirstMonth)
-                                        ),
-                                        "!bg-primary !text-white":
-                                            isEqual(date, selected[0]) || isEqual(date, selected[selected.length - 1]),
-                                        "bg-zinc-200": isSelected(date),
-                                        "border border-primary text-primary": isToday(date),
-                                    })}
+                                    onClick={() => onSelect(date)}
+                                    className={datesClassName(date, firstSundayOfFirstMonth)}
                                 >
                                     {format(date, "dd")}
                                 </div>
@@ -173,18 +191,8 @@ export default function DatepickerOne({}: DatepickerProps) {
                             {flatten(secondActiveMonth).map((date) => (
                                 <div
                                     key={format(date, "dd-MM-yyyy")}
-                                    onClick={() => onClickDate(date)}
-                                    className={classNames("w-10 h-10 flex items-center justify-center", {
-                                        "text-zinc-400": !inRange(
-                                            date,
-                                            startOfMonth(firstSundayOfSecondMonth),
-                                            endOfMonth(firstSundayOfSecondMonth)
-                                        ),
-                                        "!bg-primary !text-white":
-                                            isEqual(date, selected[0]) || isEqual(date, selected[selected.length - 1]),
-                                        "bg-zinc-200": isSelected(date),
-                                        "border border-primary text-primary": isToday(date),
-                                    })}
+                                    onClick={() => onSelect(date)}
+                                    className={datesClassName(date, firstSundayOfSecondMonth)}
                                 >
                                     {format(date, "dd")}
                                 </div>
--- src/stories/Datepicker.stories.tsx
+++ src/stories/Datepicker.stories.tsx
@@ -3,6 +3,8 @@ import DatepickerOne from "../components/DatepickerOne";
 
 export const RangeDatepicker = () => <DatepickerOne />;
 
+export const MultiDatepicker = () => <DatepickerOne type="multi" />;
+
 export default {
     title: "Datepicker",
 };
